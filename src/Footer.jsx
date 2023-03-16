ff2bb5c821fc03bce8edcd8875037e4b7e25f9af
masoumeh-talebi <masoumeh.talebi70@gmail.com>
2023-03-16 14:11:32 -0700

    init

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import { FaTrashAlt } from "react-icons/fa";
+import { FaPenFancy } from "react-icons/fa";
+
+
+
+function Footer(){
+    return(
+
+        <div>
+            <h1>khobi</h1>
+            <FaTrashAlt/>
+            <FaPenFancy/>
+        </div>
+    )
+}
+
+export default Footer;
